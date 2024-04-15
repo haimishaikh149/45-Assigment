4ab149a00093ac4904d9e32282636588bedd7009
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 22:16:55 +0500

    Add files via upload

---
+++ Q31.ts
@@ -0,0 +1,26 @@
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+
+// • If the list is empty, print the message We need to find some users!
+
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+
+let usernames: string[] = ["hamza","salman","rehan","rashid"];
+
+if(usernames.length === 0){
+    console.log(" We need to find some users!")
+}else{
+    [for(let i=0; i< usernames.length; i++);]
+}
+console.log(usernames)
+
+//2nd method
+
+let usernames1: string[] = [];
+
+if(usernames.length === 0){
+    console.log(" We need to find some users!")
+}else{
+    [for(let i=0; i< usernames.length; i++);]
+}
+console.log(usernames)
+
