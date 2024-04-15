6e5f21a58b3431126c4ce43d04529e33e2224c75
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 19:51:57 +0500

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,6 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "rashid ur rehman";
+console.log(personName.toLocaleLowerCase());
+console.log(personName.toLocaleUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(0));
