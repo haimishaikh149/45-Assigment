ae12605189786c1cb242b379dacc9dfd5c3f288f
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 19:57:05 +0500

    Add files via upload

---
+++ Q8.ts
@@ -0,0 +1,19 @@
+// You should create four lines that look like this:
+
+// _____________________________________________
+
+// console.log(5 + 3)
+
+// _____________________________________________
+
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+
+// 8
+
+console.log(5+3)//addition
+console.log(10-2)//subtraction
+console.log(2*4)//multiplication
+console.log(16/2)//division
+
+
