72cfb6cbba7573d6e5b0db72bee79d4c60f582eb
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 21:41:53 +0500

    Add files via upload

---
+++ Q20.ts
@@ -0,0 +1,9 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything
+// else you’d like. Write a program that creates a list containing these items.
+
+const cities= ["karachi", "lahor","islamabad","pishawar","hydrabad"]
+
+console.log("list of cities:")
+for(let city of cities){
+    console.log(city)
+}
