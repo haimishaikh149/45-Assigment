3a8c2c5340edca6b3b1814ec49409891d472772a
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 22:15:24 +0500

    Add files via upload

---
+++ Q28.ts
@@ -0,0 +1,106 @@
+// Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+
+// • If the person is less than 2 years old, print a message that the person is a baby.
+
+// • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+
+// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+
+// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+
+// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+
+// • If the person is age 65 or older, print a message that the person is an elder.
+ 
+
+let age1: number = 2
+if(age1<=2){
+    console.log("the person is a baby")
+}
+else if(age1>2 && age1<=4){
+    console.log(" the person is a toddler.")
+}
+else if(age1>4 && age1<=13){
+    console.log(" the person is a kid.")
+}
+else if(age1>13 && age1<=20){
+    console.log(" the person is a teenager.")
+}
+else if(age1>20 && age1<=65){
+    console.log(" the person is a adult.")
+}
+
+
+
+let age2: number = 4
+if(age2<=2){
+    console.log("the person is a baby")
+}
+else if(age2>2 && age2<=4){
+    console.log(" the person is a toolder.")
+}
+else if(age2>4 && age2<=13){
+    console.log(" the person is a kid.")
+}
+else if(age2>13 && age2<=20){
+    console.log(" the person is a teenager.")
+}
+else if(age2>20 && age2<=65){
+    console.log(" the person is a adult.")
+}
+
+
+
+
+let age3: number = 13
+if(age3<=2){
+    console.log("the person is a baby")
+}
+else if(age3>2 && age3<=4){
+    console.log(" the person is a toddler.")
+}
+else if(age3>4 && age3<=13){
+    console.log(" the person is a kid.")
+}
+else if(age3>13 && age3<=20){
+    console.log(" the person is a teenager.")
+}
+else if(age3>20 && age3<=65){
+    console.log(" the person is a adult.")
+}
+
+
+let age4: number = 20
+if(age4<=2){
+    console.log("the person is a baby")
+}
+else if(age4>2 && age4<=4){
+    console.log(" the person is a toddler.")
+}
+else if(age4>4 && age4<=13){
+    console.log(" the person is a kid.")
+}
+else if(age4>13 && age4<=20){
+    console.log(" the person is a teenager.")
+}
+else if(age4>20 && age4<=65){
+    console.log(" the person is a adult.")
+}
+
+
+let age5: number = 30
+if(age5<=2){
+    console.log("the person is a baby")
+}
+else if(age5>2 && age5<=4){
+    console.log(" the person is a toddler.")
+}
+else if(age5>4 && age5<=13){
+    console.log(" the person is a kid.")
+}
+else if(age5>13 && age5<=20){
+    console.log(" the person is a teenager.")
+}
+else if(age5>20 && age5<=65){
+    console.log(" the person is a adult.")
+}
