0364647a9a9fa589d479c434397ae969b4645f97
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 19:58:04 +0500

    Add files via upload

---
+++ Q10.ts
@@ -0,0 +1,20 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write
+// because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence
+// describing what the program does.
+
+let a = 10;
+let b = 20;
+
+//addition
+console.log(`${a}+${b} = ${a+b}`)
+//subtraction
+console.log(`${a}-${b} = ${a-b}`)
+//multiplication
+console.log(`${a}*${b} = ${a*b}`)
+//division
+console.log(`${b}/${a} = ${b/a}`)
+
+// let programName1: string = "PIAIIC";
+// let CurrentDate: string = "20.feb.2024";
+
+// console.log(`${programName1}.governour house ${CurrentDate}.it start date is`)
