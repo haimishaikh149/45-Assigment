1b239065aea3bdd122cf7e41da9c3dad9c7fe6d6
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 22:13:13 +0500

    Add files via upload

---
+++ Q24.ts
@@ -0,0 +1,58 @@
+// More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+
+// • Tests for equality and inequality with strings
+
+// • Tests using the lower case function
+
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+// • Tests using "and" and "or" operators
+
+// • Test whether an item is in a array
+
+// • Test whether an item is not in a array
+
+
+
+// • Tests for equality and inequality with strings
+
+const string1 : string =  "avacado"
+const string2 : string = "zucini"
+console.log(string1 == string2);
+console.log(string1 !== string2);
+
+// • Tests using the lower case function
+
+const MixedcaseString : string = "Hina School"
+console.log (MixedcaseString.toLocaleLowerCase()
+  === "hina school")
+  console.log(MixedcaseString.toLocaleLowerCase()
+  !== "hina school")
+
+  // • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+const num1 : number = 20;
+const num2 : number = 15;
+ console.log(num1 === num2);
+ console.log(num1 !== num2);
+ console.log(num1 < num2);
+ console.log(num1 > num2);
+ console.log(num1 <= num2);
+ console.log(num1 >= num2);
+
+
+ // • Tests using "and" and "or" operators
+
+ const Condition1 : boolean = true;
+ const Condition2 : boolean = false;
+  
+ console.log(Condition1 &&  Condition2);
+ console.log(Condition1 ||  Condition2);
+
+ // • Test whether an item is in a array
+// • Test whether an item is not in a array
+
+const fruits: string[] = ["Apple","oranges","banana","graps"];
+
+console.log(fruits.includes("Apple"))
+console.log(fruits.includes("apple"))
