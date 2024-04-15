bf35ec2ea0a0d138158dd482905b6d39140bbc1d
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 21:42:36 +0500

    Add files via upload

---
+++ Q21.js
@@ -0,0 +1,4 @@
+"use strict";
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let person = { name: "hira", fname: "ghayyas", age: 23 };
+console.log(person);
