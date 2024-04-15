83e8308c38415fddb513cccec1c3df54a40d7c2f
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 22:14:43 +0500

    Add files via upload

---
+++ Q27.js
@@ -0,0 +1,45 @@
+"use strict";
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+// • If the alien is yellow, print a message that the player earned 10 points.
+// • If the alien is red, print a message that the player earned 15 points.
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+let aliencolor = "green";
+if (aliencolor === "green") {
+    console.log(" player just earned 5 points!");
+}
+else if (aliencolor === "red") {
+    console.log("player just earned 10 points!");
+}
+else if (aliencolor === "yellow") {
+    console.log("player just earned 15 points!");
+}
+else {
+    console.log(" complet.");
+}
+let aliencolor = "red";
+if (aliencolor === "green") {
+    console.log(" player just earned 5 points!");
+}
+else if (aliencolor === "red") {
+    console.log("player just earned 10 points!");
+}
+else if (aliencolor === "yellow") {
+    console.log("player just earned 15 points!");
+}
+else {
+    console.log(" complet.");
+}
+let aliencolor = "yellow";
+if (aliencolor === "green") {
+    console.log(" player just earned 5 points!");
+}
+else if (aliencolor === "red") {
+    console.log("player just earned 10 points!");
+}
+else if (aliencolor === "yellow") {
+    console.log("player just earned 15 points!");
+}
+else {
+    console.log(" complet.");
+}
---
+++ Q27.ts
@@ -0,0 +1,52 @@
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+
+// • If the alien is green, print a message that the player earned 5 points.
+
+// • If the alien is yellow, print a message that the player earned 10 points.
+
+// • If the alien is red, print a message that the player earned 15 points.
+
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+
+let aliencolor: string= "green"
+if(aliencolor === "green"){
+    console.log(" player just earned 5 points!")
+}
+else if(aliencolor === "red"){
+    console.log("player just earned 10 points!")
+}
+ else if(aliencolor === "yellow"){
+  console.log("player just earned 15 points!")
+ }
+  else{
+    console.log(" complet.")
+  }
+
+  let aliencolor: string= "red"
+if(aliencolor === "green"){
+    console.log(" player just earned 5 points!")
+}
+else if(aliencolor === "red"){
+    console.log("player just earned 10 points!")
+}
+ else if(aliencolor === "yellow"){
+  console.log("player just earned 15 points!")
+ }
+  else{
+    console.log(" complet.")
+  }
+
+  let aliencolor: string= "yellow"
+if(aliencolor === "green"){
+    console.log(" player just earned 5 points!")
+}
+else if(aliencolor === "red"){
+    console.log("player just earned 10 points!")
+}
+ else if(aliencolor === "yellow"){
+  console.log("player just earned 15 points!")
+ }
+  else{
+    console.log(" complet.")
+  }
