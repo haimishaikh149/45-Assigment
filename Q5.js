81dc127d2e82bda9c10924b55f3b5e5a3d380401
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 19:53:03 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,4 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "HAZRAT ALI once said";
+var message = "Shower all your love on your friend, but do not shower all of your trust on him. -Hazrat Ali A.S";
+console.log(" ".concat(famous_person, " , ").concat(message));
