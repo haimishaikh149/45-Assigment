60a21419c4c782b38c215af5b4b40e5143ccf51b
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 21:43:51 +0500

    Add files via upload

---
+++ Q23.js
@@ -0,0 +1,49 @@
+"use strict";
+// Conditional Tests: Write a series of conditional tests. Print a statement
+// describing each test and your prediction for the results of each test. Your code
+// should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let name1 = 'hira';
+console.log("is name == 'hira'?  i prdicte true. otherwise i prdict false");
+console.log(name1 === 'hira');
+console.log(name1 === 'tasken');
+let dress = 'black';
+console.log("is dress == 'black'?  i prdicte true. otherwise i prdict false");
+console.log(dress === 'black');
+console.log(dress === 'yellow');
+let car = 'civic';
+console.log("is car== 'civic'?  i prdicte true. otherwise i prdict false");
+console.log(car === 'car');
+console.log(car === 'audi');
+let vichale = 'land crouser';
+console.log("is vichale == 'land crouser'?  i prdicte true. otherwise i prdict false");
+console.log(vichale === 'land crouser');
+console.log(vichale === 'hilax');
+let colr = 'orange';
+console.log("is colr == 'orange'?  i prdicte true. otherwise i prdict false");
+console.log(colr === 'orange');
+console.log(colr === 'bnana');
+let laptop = 'hp';
+console.log("is laptop== 'hp'?  i prdicte true. otherwise i prdict false");
+console.log(laptop === 'hp');
+console.log(laptop === 'apple');
+let jelly = 'chilli milli';
+console.log("is jelly == 'chilli milli'?  i prdicte true. otherwise i prdict false");
+console.log(jelly === 'chilli milli');
+console.log(jelly === 'tasken');
+let bike = 'heavy bike';
+console.log("is bike == 'heavy bike'?  i prdicte true. otherwise i prdict false");
+console.log(bike === 'heavy bike');
+console.log(bike === '150 bike');
+let num = '12345';
+console.log("is num == '12345'?  i prdicte true. otherwise i prdict false");
+console.log(num === '12345');
+console.log(num === '0000');
+let city = 'karachi';
+console.log("is city == 'karachi'?  i prdicte true. otherwise i prdict false");
+console.log(city === 'karachi');
+console.log(city === 'islamabad');
---
+++ Q23.ts
@@ -0,0 +1,66 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement
+// describing each test and your prediction for the results of each test. Your code
+// should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+
+ 
+
+let name1: any = 'hira'
+ console.log("is name == 'hira'?  i prdicte true. otherwise i prdict false")
+ console.log(name1 === 'hira')
+ console.log(name1 === 'tasken')
+
+ let dress: any = 'black'
+ console.log("is dress == 'black'?  i prdicte true. otherwise i prdict false")
+ console.log(dress === 'black')
+ console.log(dress === 'yellow')
+
+ let car: any = 'civic'
+ console.log("is car== 'civic'?  i prdicte true. otherwise i prdict false")
+ console.log(car === 'car')
+ console.log(car === 'audi')
+ 
+ let vichale: any = 'land crouser'
+ console.log("is vichale == 'land crouser'?  i prdicte true. otherwise i prdict false")
+ console.log(vichale === 'land crouser')
+ console.log(vichale === 'hilax')
+
+ let colr: any = 'orange'
+ console.log("is colr == 'orange'?  i prdicte true. otherwise i prdict false")
+ console.log(colr === 'orange')
+ console.log(colr === 'bnana')
+
+ let laptop: any = 'hp'
+ console.log("is laptop== 'hp'?  i prdicte true. otherwise i prdict false")
+ console.log(laptop === 'hp')
+ console.log(laptop === 'apple')
+
+ let jelly: any = 'chilli milli'
+ console.log("is jelly == 'chilli milli'?  i prdicte true. otherwise i prdict false")
+ console.log(jelly === 'chilli milli')
+ console.log(jelly === 'tasken')
+
+ let bike: any = 'heavy bike'
+ console.log("is bike == 'heavy bike'?  i prdicte true. otherwise i prdict false")
+ console.log(bike === 'heavy bike')
+ console.log(bike === '150 bike')
+
+ let num: any = '12345'
+ console.log("is num == '12345'?  i prdicte true. otherwise i prdict false")
+ console.log(num === '12345')
+ console.log(num === '0000')
+
+ let city: any = 'karachi'
+ console.log("is city == 'karachi'?  i prdicte true. otherwise i prdict false")
+ console.log(city === 'karachi')
+ console.log(city === 'islamabad')
