5abd265dcb6000d2eb5b09033919c953dfb97377
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-15 21:41:26 +0500

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,6 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number
+// of people you are inviting to dinner.
+let invitations = ["hamza", "salman bhai", "rashid", "usman"];
+let count_invitation = invitations.length;
+console.log(`${count_invitation} people will come to the dinner`);
---
+++ Q19.ts
@@ -0,0 +1,7 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number
+// of people you are inviting to dinner.
+
+let invitations:string[] = ["hamza","salman bhai","rashid","usman"]
+let count_invitation : number = invitations.length
+
+console.log(`${count_invitation} people will come to the dinner`)
